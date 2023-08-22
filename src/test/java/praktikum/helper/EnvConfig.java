3278d041d031f1930e414e83b3f3fb55d9009c48
Maria Belova <bmvria357@gmail.com>
2023-08-23 01:29:54 +0300

    add EnvConfig

---
+++ src/test/java/praktikum/helper/EnvConfig.java
@@ -0,0 +1,9 @@
+package praktikum.helper;
+
+public class EnvConfig {
+    public static final String BASE_URL = "https://qa-scooter.praktikum-services.ru/";
+    public static final int TIMEOUT_DEFAULT = 10;
+    public static final int TIMEOUT_LONG = 30;
+    public static final String WEB_DRIVER_CHROME = "C:/WebDriver/bin/chromedriver";
+    public static final String WEB_DRIVER_FIREFOX = "C:/WebDriver/bin/geckodriver";
+}
