16ad4ff65bab00aa81291ea0c5f7ef6c50d94b34
Maria Belova <bmvria357@gmail.com>
2023-09-07 21:02:40 +0300

    добавила отступ

--- src/test/java/praktikum/helper/EnvConfig.java
+++ src/test/java/praktikum/helper/EnvConfig.java
@@ -4,6 +4,7 @@ public class EnvConfig {
     public static final String BASE_URL = "https://qa-scooter.praktikum-services.ru/";
     public static final int TIMEOUT_DEFAULT = 10;
     public static final int TIMEOUT_LONG = 30;
+
     public static final String WEB_DRIVER_CHROME_BIN =  "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome";
     public static final String WEB_DRIVER_CHROME = System.getenv("projects_dir") + "/libs/drivers/chrome/chromedriver";
 
