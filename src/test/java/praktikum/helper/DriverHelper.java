28344d3feb8aa2372de0c5283a84aa6925a84738
Maria Belova <bmvria357@gmail.com>
2023-09-07 20:51:08 +0300

    Переход на macOS

--- src/test/java/praktikum/helper/DriverHelper.java
+++ src/test/java/praktikum/helper/DriverHelper.java
@@ -10,8 +10,7 @@ import org.openqa.selenium.firefox.GeckoDriverService;
 import java.io.File;
 import java.time.Duration;
 
-import static praktikum.helper.EnvConfig.WEB_DRIVER_CHROME;
-import static praktikum.helper.EnvConfig.WEB_DRIVER_FIREFOX;
+import static praktikum.helper.EnvConfig.*;
 
 public class DriverHelper extends ExternalResource {
 
@@ -30,18 +29,25 @@ public class DriverHelper extends ExternalResource {
     private void setUpChrome() {
         System.setProperty("webdriver.http.factory", "jdk-http-client");
         ChromeDriverService service = new ChromeDriverService.Builder()
+                .withLogOutput(System.out)
                 .usingDriverExecutable(new File(WEB_DRIVER_CHROME))
                 .build();
-        driver = new ChromeDriver(service);
+        ChromeOptions options = new ChromeOptions()
+                .setBinary(WEB_DRIVER_CHROME_BIN);
+        driver = new ChromeDriver(service, options);
     }
 
-    public void setUpFirefox() {
-        System.setProperty("webdriver.http.factory", "jdk-http-client");
-        var service = new GeckoDriverService.Builder()
-                .usingDriverExecutable(new File(WEB_DRIVER_FIREFOX))
-                .build();
-        driver = new FirefoxDriver(service);
-    }
+        public void setUpFirefox () {
+            System.setProperty("webdriver.http.factory", "jdk-http-client");
+            var service = new GeckoDriverService.Builder()
+                    .withLogOutput(System.out)
+                    .usingDriverExecutable(new File(WEB_DRIVER_FIREFOX))
+                    .build();
+            FirefoxOptions options = new FirefoxOptions()
+                    .setBinary(WEB_DRIVER_FIREFOX_BIN);
+
+            driver = new FirefoxDriver(service, options);
+        }
 
     @Override
     protected void after() {
--- src/test/java/praktikum/helper/EnvConfig.java
+++ src/test/java/praktikum/helper/EnvConfig.java
@@ -4,6 +4,9 @@ public class EnvConfig {
     public static final String BASE_URL = "https://qa-scooter.praktikum-services.ru/";
     public static final int TIMEOUT_DEFAULT = 10;
     public static final int TIMEOUT_LONG = 30;
-    public static final String WEB_DRIVER_CHROME = "C:/WebDriver/bin/chromedriver";
-    public static final String WEB_DRIVER_FIREFOX = "C:/WebDriver/bin/geckodriver";
+    public static final String WEB_DRIVER_CHROME_BIN =  "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome";
+    public static final String WEB_DRIVER_CHROME = System.getenv("projects_dir") + "/libs/drivers/chrome/chromedriver";
+
+    public static final String WEB_DRIVER_FIREFOX_BIN =  "/Applications/Firefox.app/Contents/MacOS/Firefox";
+    public static final String WEB_DRIVER_FIREFOX = System.getenv("projects_dir") + "/libs/drivers/firefox/geckodriver";
 }
